438891d3e37812e8ffaf36485d711e1c5568bd58
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-05-06 12:17:45 -0300

    Initial commit

---
+++ 01-Ejercicio1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Ejercicio1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int divisor = 2;
+            List<int> numeros = NumerosDivisiblesPorUnNumero(1000, divisor);
+
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero + " es divisible por " + divisor);
+            }
+        }
+
+        private static List<int> NumerosDivisiblesPorUnNumero(int numeroMaximo, int divisor)
+        {
+            List<int> numeros = new List<int>();
+            
+            //TODO: Solicitar este valor por teclado con Console.ReadLine
+            //y validar que el valor ingresado sea un número
+            //int divisor = 2;
+
+            for (int i = 1; i <= numeroMaximo; i++)
+            {
+                if (i % divisor == 0)
+                {
+                    //Console.WriteLine(i + " es divisible por 3");
+                    numeros.Add(i);
+                }
+            }
+
+            return numeros;
+        }
+    }
+}
---
+++ 02-Ejercicio2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02_Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero mayor a 1");
+
+            string valorIngresado = Console.ReadLine();
+            
+            if (ValidarNumeroIngresado(valorIngresado) == false)
+            {
+                Console.WriteLine("El valor ingresado NO es un número");
+                System.Environment.Exit(1);
+            }
+
+            int number = int.Parse(valorIngresado);
+
+            if (ValidarNumeroMayorAUno(number) == false)
+            {
+                Console.WriteLine("El valor ingresado DEBE ser mayor a 1");
+                System.Environment.Exit(1);
+            }
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            };
+        }
+
+        static bool ValidarNumeroIngresado(string valorIngresado)
+        {
+            string expresionRegular = @"^[0-9]+$";
+            if (Regex.IsMatch(valorIngresado, expresionRegular) == false)
+            {
+                //Console.WriteLine("El valor ingresado NO es un número");
+                //System.Environment.Exit(1);
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool ValidarNumeroMayorAUno(int valorIngresado)
+        {
+            if (valorIngresado <= 1)
+            {
+                //Console.WriteLine("El valor ingresado DEBE ser mayor a 1");
+                //System.Environment.Exit(2);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
